2d7619a8f1785fa796a81024153ed3a877d01b81
VaishnaviiSanju <vaishoff@gmail.com>
2021-08-11 00:24:40 +0530

    Master pushes

---
+++ src/org/java/JavaGit.java
@@ -0,0 +1,17 @@
+package org.java;
+
+public class JavaGit {
+	private void add() {
+		System.out.println("add");
+
+	}
+	private void sub() {
+		System.out.println("sub");
+		System.out.println("Multiply");
+	}
+	public static void main(String[] args) {
+		JavaGit c = new JavaGit();
+		c.add();
+	}
+	}
+
